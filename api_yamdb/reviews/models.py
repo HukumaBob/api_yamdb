b32a0f64a325d062ff37c97764edba0272f2842a
Nikita <ldc.endoscopy@gmail.com>
2023-03-29 09:40:24 +0400

    dummy_custom_user_01

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
+class CustomUser(AbstractUser):
+    bio = models.TextField(
+        'Биография',
+        blank=True,
+    )
 
 # Create your models here.
