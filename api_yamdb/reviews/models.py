2c46f00a1bcf86bfab0db34b350f51c5397b8140
Nikita <ldc.endoscopy@gmail.com>
2023-03-29 11:42:15 +0400

    Add roles to user model and dummy classes in models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,9 +1,43 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
 class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('user', 'User'),
+        ('moderator', 'Moderator'),
+        ('admin', 'Admin'),
+    )
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default='user',
+    )
     bio = models.TextField(
-        'Описание',
+        'Description',
         blank=True,
     )
+
+
+User = get_user_model()
+
+
+class Category(models.Model):
+    pass
+
+
+class Genre(models.Model):
+    pass
+
+
+class Title(models.Model):
+    pass
+
+
+class Reviews(models.Model):
+    pass
+
+
+class Comments(models.Model):
+    pass
